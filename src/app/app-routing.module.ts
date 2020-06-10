09bb153583a819eb70ae2021d79cc17808e89c8d
edy1717 <58958795+edy1717@users.noreply.github.com>
2020-06-09 23:08:34 -0500

    nueva2.1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,21 +10,18 @@ import { ProfileComponent } from './components/profile/profile/profile.component
 import { MyproductsComponent } from './components/profile/myproducts/myproducts.component';
 import { ReportsComponent } from './components/profile/reports/reports.component';
 import { AvisoPrivacidadComponent } from './components/aviso-privacidad/aviso-privacidad.component';
-import { ModalMapsComponent } from './components/modals/modal-maps/modal-maps.component';
 
 
 const routes: Routes = [
   { path : 'home', component : HomeComponent , pathMatch: 'full'},
-  { path : 'maps', component : ModalMapsComponent},
   { path : 'aviso-de-privavidad', component : AvisoPrivacidadComponent },
   { path : 'user/login' , component : LoginComponent },
-  { path : 'user/profile' ,  component : ProfileComponent ,canActivate: [AuthGuard]  },
+  { path : 'user/profile' ,  component : ProfileComponent ,canActivate: [AuthGuard]},
   { path : 'admin/list-products', component : ListProductsComponent, canActivate: [AuthGuard] },
   { path : 'admin/list-profiles', component :ListProfilesComponent, canActivate: [AuthGuard] },
   { path : 'admin/offers', component : OffersComponent, canActivate: [AuthGuard] },
   { path : 'admin/reports', component : ReportsComponent, canActivate: [AuthGuard]},
   { path : 'admin/user-products/:usuarioCreador', component : MyproductsComponent, canActivate: [AuthGuard] },
-  { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path : '***',  component : HomeComponent }
 
 ];
--- src/app/services/cultivo.service.ts
+++ src/app/services/cultivo.service.ts
@@ -1,9 +1,7 @@
 import { Injectable } from '@angular/core';
 import { environment } from '../../environments/environment';
 import { ProductInterface } from '../interfaces/products';
-import {map} from 'rxjs/operators';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { AuthService } from './auth.service';
 
 
 const httpOptions = { headers: new HttpHeaders({ "Content-Type": "application/json" ,
@@ -26,7 +24,7 @@ export class CultivoService {
     medida: '',
 };
 
-  constructor( private http: HttpClient, private authService: AuthService ) {
+  constructor( private http: HttpClient) {
 
   }
 
@@ -40,7 +38,7 @@ export class CultivoService {
 
 
   consultarTodosCultivos(){
-    return this.http.get(this.url + '/obtener/todos', httpOptions);
+    return this.http.get(this.url + '/obtener/todos');
   }
 
   editarCultivo(data){
@@ -52,3 +50,6 @@ export class CultivoService {
      return this.http.post(this.url + '/eliminar', id, httpOptions);
 }
 }
+
+
+
